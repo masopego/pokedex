7e3d649533512b9f9451bdf8b24ccdd6dae11652
masopego <masopego@gmail.com>
2025-01-06 18:27:37 +0100

    Add sign up validations

--- app/src/main/java/com/penagomez/pokedex/RegisterActivity.java
+++ app/src/main/java/com/penagomez/pokedex/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.penagomez.pokedex;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -14,6 +15,8 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
     FirebaseAuth auth = FirebaseAuth.getInstance();
+
+    int PASSWORD_MAX_LENTH = 6;
 
     Button registerButton;
     EditText emailEditText;
@@ -37,21 +40,33 @@ public class RegisterActivity extends AppCompatActivity {
         String email = emailEditText.getText().toString().trim();
         String password = passwordEditText.getText().toString().trim();
 
-        if (!email.isEmpty() && !password.isEmpty()) {
-            auth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(task -> {
-                        if (task.isSuccessful()) {
-                            FirebaseUser user = auth.getCurrentUser();
-                            Toast.makeText(this, "Usuario registrado correctamente " + user.getEmail(), Toast.LENGTH_SHORT).show();
 
-                            goToLoginActivity();
-                        } else {
-                            Toast.makeText(this, "Authentication Failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    });
-        } else {
-            Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this,  R.string.required_error, Toast.LENGTH_SHORT).show();
+            return;
         }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(this, R.string.invalid_format_error, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (password.length() < PASSWORD_MAX_LENTH) {
+            Toast.makeText(this,  R.string.max_length_error, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = auth.getCurrentUser();
+                        Toast.makeText(this, R.string.user_register_success + user.getEmail(), Toast.LENGTH_SHORT).show();
+
+                        goToLoginActivity();
+                    } else {
+                        Toast.makeText(this, R.string.generic_error + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
 
     }
 
